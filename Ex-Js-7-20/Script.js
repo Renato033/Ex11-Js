cbb2ad08333da83878075c4d0a7a4f004b6933be
Renato033 <rg202436@gmail.com>
2026-04-05 20:46:13 -0300

    Add files via upload

---
+++ Ex-Js-7-20/Script.js
@@ -0,0 +1,5 @@
+console.log("conversão-dolar!");
+const valorEmReais = Number(prompt("Digite o valor em reais:"));
+console.clear()
+console.log("SEUS DOLARES")
+console.log("Voce tem $${(valor em reais / 5.30).tofixed(2)}")
